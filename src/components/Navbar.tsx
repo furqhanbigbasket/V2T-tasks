3e5b37268843afec243acc9378e814932015d8aa
furqhanbigbasket <furqhanbigbasket@gmail.com>
2024-03-22 05:14:34 +0530

    Your commit message here

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,39 +2,72 @@
 import { Link, useNavigate } from "react-router-dom";
 import { PencilIcon } from "@heroicons/react/24/solid";
 import Button from "./Button";
+import Select from "./Select";
+import { useEffect, useState } from "react";
+
 
 const Navbar = () => {
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	return (
-		<nav className="py-4 border-b-2 border-container shadow-md shadow-gray-400 w-full fixed top-0 bg-base">
-			<ul className="flex items-center justify-between  w-11/12 mx-auto">
-				<Link to="/">
-					<Button	>
-<span className="font-semibold flex gap-2 justify-center items-center  text-main">
-<span>
-Taskwrite
-	</span>
-<PencilIcon  height={24} width={24}  >
+    const themeArray = ["light", "dark", "system"];
+    const [theme, setTheme] = useState(() => {
+    	return localStorage.getItem("theme") || themeArray[2];
+    });
 
-</PencilIcon>
-</span>
-						</Button>
-				</Link>
+    const applyTheme = (selectedTheme: string) => {
+        const isDarkModePreferred = window.matchMedia(
+            "(prefers-color-scheme: dark)"
+            ).matches;
 
+        document.documentElement.classList.remove("light", "dark");
+        document.documentElement.classList.add(selectedTheme);
 
-				{/* <Link  to="/home"> */}
-				<div className="flex items-center justify-between gap-6">
-					<Link
-						to="/tasks"
-						className="font-semibold hover:scale-105 transition duration-300 ease-in-out"
-					>
-						View Tasks
-					</Link>
-				</div>
-			</ul>
-		</nav>
-	);
+        if (selectedTheme === "system") {
+        document.documentElement.classList.toggle("dark", isDarkModePreferred);
+        document.documentElement.classList.toggle("light", 		 		!isDarkModePreferred);
+        }
+    };
+
+    const handleSelectTheme = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedTheme = e.target.value;
+        setTheme(selectedTheme);
+
+        // Store the selected theme in localStorage
+        localStorage.setItem("theme", selectedTheme);
+    };
+
+    useEffect(() => {
+    	applyTheme(theme);
+    }, [theme]);
+
+    return (
+        <nav className="py-4 border-b-2 border-container shadow-md shadow-gray-400 w-full fixed top-0 bg-base">
+            <ul className="flex items-center justify-between  w-11/12 mx-auto">
+                <Link to="/">
+                    <Button>
+                        <span className="font-semibold text-main">Taskwrite</span>
+                        <PencilIcon height={20} className="text-main" />
+                    </Button>
+                </Link>
+                <div className="flex items-center justify-between gap-6">
+                <Link
+                    to="/tasks"
+                    className="font-semibold hover:scale-105 transition duration-300 ease-in-out"
+                >
+                	View Tasks
+                </Link>
+                <div className="flex gap-2 items-center">
+                    <span className="font-semibold"> Theme: </span>
+                    <Select
+                        defaultSelectValue={theme}
+                        selectOptions={themeArray}
+                        handleSelectChange={handleSelectTheme}
+                    />
+                </div>
+                </div>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -23,8 +23,8 @@ const Index = () => {
 
 	return (
 		<main className="container mx-auto">
-			<section className="max-w-5xl mx-auto m-12 p-16">
-				<h1 className="text-4xl md:text-7xl font-bold text-center py-3 mb-16">
+			<section className="max-w-5xl mx-auto m-12 pt-12 ">
+				<h1 className="text-4xl md:text-7xl font-bold text-center py-3  mb-16">
 					AI-enhanced, Voice-enabled, Searchable Task Manager
 				</h1>
 
